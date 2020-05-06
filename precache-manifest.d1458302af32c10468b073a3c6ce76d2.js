6a5ddca1ae491dfb897d6d28dcd1b2dcc0d275e4
ashar <ashar.ansari1020@gmail.com>
2020-05-06 10:19:25 +0530

    Updates

---
+++ precache-manifest.d1458302af32c10468b073a3c6ce76d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b273c7a0cd6a049ff0841d258b380679",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "5b448177ca50a740c4f2",
+    "url": "/my-react-portfolio/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "919ea6fa7ce7eb2822c3",
+    "url": "/my-react-portfolio/static/js/2.ba615d4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-portfolio/static/js/2.ba615d4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b448177ca50a740c4f2",
+    "url": "/my-react-portfolio/static/js/main.65f2688e.chunk.js"
+  },
+  {
+    "revision": "35f9e58eb32da499e28b",
+    "url": "/my-react-portfolio/static/js/runtime-main.880269f4.js"
+  },
+  {
+    "revision": "7b2270cec46a2e907afb3caa3ac5052d",
+    "url": "/my-react-portfolio/static/media/about.7b2270ce.png"
+  },
+  {
+    "revision": "bcb3f2a650b6dadf092a1102ce1523c9",
+    "url": "/my-react-portfolio/static/media/img-3.bcb3f2a6.jpg"
+  },
+  {
+    "revision": "bce76e1949031a2862923f3addafc5d2",
+    "url": "/my-react-portfolio/static/media/s1.bce76e19.jpg"
+  }
+]);
